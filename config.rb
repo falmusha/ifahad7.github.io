dd185302cdd3381fe72785527b5639aa439b8936
Fahad Almusharraf <ifahad7@gmail.com>
2016-01-01 20:52:38 -0500

    update config file

--- config.rb
+++ config.rb
@@ -41,7 +41,7 @@ end
 # Build-specific configuration
 configure :build do
   # Minify CSS on build
-  ctivate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
